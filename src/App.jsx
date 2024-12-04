430728bd7474eb95cf9056b9fda190816632defe
Samuel Gonçalves <samuelesilva@hotmail.com>
2024-12-04 13:51:02 -0300

    Criando formulário de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 // pages
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
-
-//components
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 function App() {
   return (
@@ -19,6 +19,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path='/register' element={<Register />} />
+
           </Routes>
         </div>
         <Footer />
