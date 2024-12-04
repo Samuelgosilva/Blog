bb8c67b6d62f2ea9d16c835edb25209b9de36600
Samuel Gonçalves <samuelesilva@hotmail.com>
2024-12-04 16:19:19 -0300

    States do form de cadastro

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,14 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
+
+    
     <footer className={styles.footer}>
         <h3>Ecreva sobre o que você tem interesse!</h3>
         <p>Mini Blog &copy; 2024</p>
     </footer>
+
+    
   )
 }
 
